fc992f5ee18b57f14a91a9357ed5964787b95d4c
Infinite coder <123761858+Infinite-Coder-001@users.noreply.github.com>
2023-02-19 17:20:24 +0100

    [BOT] Create api.js

---
+++ api.js
@@ -0,0 +1,244 @@
+function HotlistAPI() {
+  return [
+    {
+      title: 'Stubborn', 
+      author: 'NL#Midsane', 
+      votes: 62, 
+      forks: 1, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/stubborn/5376267191107584/5541483973492736.png', 
+      link: 'https://www.khanacademy.org/computer-programming/stubborn/5376267191107584'
+    }, 
+    {
+      title: 'Spin-off of "Project: Fish tank"', 
+      author: 'MR DMJC', 
+      votes: 26, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/spin-off-of-project-fish-tank/5224854897082368/5194177287864320.png', 
+      link: 'https://www.khanacademy.org/computer-programming/spin-off-of-project-fish-tank/5224854897082368'
+    }, 
+    {
+      title: 'coded by AI', 
+      author: 'apex', 
+      votes: 19, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/coded-by-ai/5502867022856192/6150176402358272.png', 
+      link: 'https://www.khanacademy.org/computer-programming/coded-by-ai/5502867022856192'
+    }, 
+    {
+      title: 'The ways to destroy OhNoes.', 
+      author: 'Wort-Head Productions', 
+      votes: 71, 
+      forks: 4, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/the-ways-to-destroy-ohnoes/5334630865879040/6523759704195072.png', 
+      link: 'https://www.khanacademy.org/computer-programming/the-ways-to-destroy-ohnoes/5334630865879040'
+    }, 
+    {
+      title: 'H O P E - NF', 
+      author: 'silverleaf12 [Romans 8:31-39]', 
+      votes: 57, 
+      forks: 2, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/h-o-p-e-nf/6322114015838208/6564559913795584.png', 
+      link: 'https://www.khanacademy.org/computer-programming/h-o-p-e-nf/6322114015838208'
+    }, 
+    {
+      title: 'Fencing [GAME]', 
+      author: 'Phase_009', 
+      votes: 33, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/fencing-game/6697938948767744/6104200461467648.png', 
+      link: 'https://www.khanacademy.org/computer-programming/fencing-game/6697938948767744'
+    }, 
+    {
+      title: 'link official release (animated)', 
+      author: 'F', 
+      votes: 17, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/link-official-release-animated/4698741535981568/5795385132236800.png', 
+      link: 'https://www.khanacademy.org/computer-programming/link-official-release-animated/4698741535981568'
+    }, 
+    {
+      title: 'challenge three: favorite movie', 
+      author: 'Clark Kent (offline)', 
+      votes: 19, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/challenge-three-favorite-movie/6644321113915392/5313368334581760.png', 
+      link: 'https://www.khanacademy.org/computer-programming/challenge-three-favorite-movie/6644321113915392'
+    }, 
+    {
+      title: 'Pro Clicker', 
+      author: 'Dark Knight', 
+      votes: 54, 
+      forks: 5, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/pro-clicker/5402081313996800/6725065962209280.png', 
+      link: 'https://www.khanacademy.org/computer-programming/pro-clicker/5402081313996800'
+    }, 
+    {
+      title: 'mew porgam', 
+      author: 'mr. mustache', 
+      votes: 19, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/mew-porgam/6094225326522368/6127676014510080.png', 
+      link: 'https://www.khanacademy.org/computer-programming/mew-porgam/6094225326522368'
+    }, 
+    {
+      title: 'Triangle Subdivision', 
+      author: 'Trevor Hoffman', 
+      votes: 7, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/triangle-subdivision/5840437862776832/5612101764956160.png', 
+      link: 'https://www.khanacademy.org/computer-programming/triangle-subdivision/5840437862776832'
+    }, 
+    {
+      title: 'Gentlemanly Caterpillar', 
+      author: 'ChandraMCA', 
+      votes: 20, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/gentlemanly-caterpillar/4688020425195520/6010266372587520.png', 
+      link: 'https://www.khanacademy.org/computer-programming/gentlemanly-caterpillar/4688020425195520'
+    }, 
+    {
+      title: 'maybe my intro', 
+      author: '', 
+      votes: 46, 
+      forks: 1, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/maybe-my-intro/5892270863990784/5560656042704896.png', 
+      link: 'https://www.khanacademy.org/computer-programming/maybe-my-intro/5892270863990784'
+    }, 
+    {
+      title: 'entry for ExDiCode challenge three', 
+      author: 'F', 
+      votes: 8, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/entry-for-exdicode-challenge-three/6722696153186304/5076757143404544.png', 
+      link: 'https://www.khanacademy.org/computer-programming/entry-for-exdicode-challenge-three/6722696153186304'
+    }, 
+    {
+      title: 'Squish Oh-Noes', 
+      author: 'TheGreatPizzaMan', 
+      votes: 52, 
+      forks: 8, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/squish-oh-noes/4700629067153408/6669672298889216.png', 
+      link: 'https://www.khanacademy.org/computer-programming/squish-oh-noes/4700629067153408'
+    }, 
+    {
+      title: 'Helpful Resources', 
+      author: 'Dipper98', 
+      votes: 12, 
+      forks: 2, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/helpful-resources/6040514948546560/5341248974274560.png', 
+      link: 'https://www.khanacademy.org/computer-programming/helpful-resources/6040514948546560'
+    }, 
+    {
+      title: 'Shamos's Algorithm', 
+      author: 'ethankim8683', 
+      votes: 4, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/shamoss-algorithm/5496319110332416/5786495153913856.png', 
+      link: 'https://www.khanacademy.org/computer-programming/shamoss-algorithm/5496319110332416'
+    }, 
+    {
+      title: 'Hyperspace Hype', 
+      author: 'Bearkirb314', 
+      votes: 3, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/hyperspace-hype/6467857892556800/5180839663288320.png', 
+      link: 'https://www.khanacademy.org/computer-programming/hyperspace-hype/6467857892556800'
+    }, 
+    {
+      title: 'Vaporeon Pixel Art', 
+      author: 'Eryn #TeamUmbra', 
+      votes: 9, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/vaporeon-pixel-art/5274559138021376/4621265285726208.png', 
+      link: 'https://www.khanacademy.org/computer-programming/vaporeon-pixel-art/5274559138021376'
+    }, 
+    {
+      title: 'DA Winston Generator 2.0', 
+      author: 'DeGoatMelon', 
+      votes: 24, 
+      forks: 2, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/da-winston-generator-20/5426295634968576/6447258803683328.png', 
+      link: 'https://www.khanacademy.org/computer-programming/da-winston-generator-20/5426295634968576'
+    }, 
+    {
+      title: 'MineKhan + More Block Designs & Shapes!', 
+      author: 'Aragorn Elessar (Offline)', 
+      votes: 5, 
+      forks: 4, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/minekhan-more-block-designs-shapes/4547490303229952/6540491621711872.png', 
+      link: 'https://www.khanacademy.org/computer-programming/minekhan-more-block-designs-shapes/4547490303229952'
+    }, 
+    {
+      title: 'Code Art self-portrait-2023', 
+      author: 'angelts2010', 
+      votes: 10, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/code-art-self-portrait-2023/5207268213964800/6611376036691968.png', 
+      link: 'https://www.khanacademy.org/computer-programming/code-art-self-portrait-2023/5207268213964800'
+    }, 
+    {
+      title: 'clean ur screen', 
+      author: 'Reader ', 
+      votes: 6, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/clean-ur-screen/5024356227137536/4938699741904896.png', 
+      link: 'https://www.khanacademy.org/computer-programming/clean-ur-screen/5024356227137536'
+    }, 
+    {
+      title: 'Click-Through Format', 
+      author: 'Agent Bob', 
+      votes: 5, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/click-through-format/4972825612173312/5733257155231744.png', 
+      link: 'https://www.khanacademy.org/computer-programming/click-through-format/4972825612173312'
+    }, 
+    {
+      title: 'Smooth Backgrounds', 
+      author: 'Agent Bob', 
+      votes: 5, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/smooth-backgrounds/5724764522201088/4754552689836032.png', 
+      link: 'https://www.khanacademy.org/computer-programming/smooth-backgrounds/5724764522201088'
+    }, 
+    {
+      title: 'star girl', 
+      author: 'Natalie Bartlett', 
+      votes: 6, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/star-girl/4770371859693568/5580996940709888.png', 
+      link: 'https://www.khanacademy.org/computer-programming/star-girl/4770371859693568'
+    }, 
+    {
+      title: 'Blobed | Game - CoC 2023', 
+      author: 'Astro', 
+      votes: 87, 
+      forks: 8, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/blobed-game-coc-2023/5710788212080640/4861799057113088.png', 
+      link: 'https://www.khanacademy.org/computer-programming/blobed-game-coc-2023/5710788212080640'
+    }, 
+    {
+      title: 'Launch missiles at Oh-Noes!', 
+      author: 'proGamer194', 
+      votes: 22, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/launch-missiles-at-oh-noes/5027293777149952/5440614183518208.png', 
+      link: 'https://www.khanacademy.org/computer-programming/launch-missiles-at-oh-noes/5027293777149952'
+    }, 
+    {
+      title: 'KA language practice', 
+      author: 'Kopter Coding', 
+      votes: 4, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/ka-language-practice/5264137852862464/6109745190846464.png', 
+      link: 'https://www.khanacademy.org/computer-programming/ka-language-practice/5264137852862464'
+    }, 
+    {
+      title: '', 
+      author: 'crazy__codes', 
+      votes: 8, 
+      forks: 3, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/webpage/5246132091666432/5166991011004416.png', 
+      link: 'https://www.khanacademy.org/computer-programming/webpage/5246132091666432'
+    }, 
+  ];
+}
