7746d633119b32aed7f9e8b1ead8d280c76d739f
Infinite coder <123761858+Infinite-Coder-001@users.noreply.github.com>
2023-03-09 16:03:30 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,44 @@
+// This is the main file, that is loading in the <head> of your HTML page
+// Link to this file is 'https://cdn.jsdelivr.net/gh/Infinite-Coder-001/Hotlist-API@main/main.js'
+
+function HotlistAPI(callback) {
+    function loadHotlistData2(delay) {
+        var timeoutVariable;
+        function timeout(delay) {
+            loadHotlistData2(delay + 10);
+        }
+        function finished() {
+            clearTimeout(timeoutVariable);
+            callback(HotlistAPI());
+        }
+        var script = document.createElement('script');
+        var date = new Date(Date.now() - 1000 * 60 * delay);
+        var year = date.getUTCFullYear();
+        var month = date.getUTCMonth();
+        var day = date.getUTCDate();
+        var hours = date.getUTCHours();
+        var minutes = date.getUTCMinutes();
+
+        if (String(month).split("").length === 1) {
+            month = "0" + String(parseInt(month) + 1);
+        }
+        if (String(day).split("").length === 1) {
+            day = "0" + day;
+        }
+        if (String(hours).split("").length === 1) {
+            hours = "0" + hours;
+        }
+        minutes = String(minutes).split("")[0];
+
+        script.src = "https://cdn.jsdelivr.net/gh/Infinite-Coder-001/Hotlist-API@main/api-" + year + month + day + hours + minutes + "0.js";
+        script.onload = function() {
+            finished();
+        };
+
+        document.head.appendChild(script); 
+        timeoutVariable = setTimeout(function() {
+            timeout(delay);
+        }, 100);
+    }
+    loadHotlistData2(0);
+}
