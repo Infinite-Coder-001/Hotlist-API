b9bfe7e3d8ff470bc04dfaf2a347ef7196bcab2a
Infinite-Coder-001 <Infinite-Coder-001@users.noreply.github.com>
2023-03-09 02:37:00 +0000

    Automated Change

---
+++ api-202303090230.js
@@ -0,0 +1,244 @@
+var HotlistAPI = function() {
+  return [
+    {
+      title: 'Skinamarink', 
+      author: 'soap.', 
+      votes: 39, 
+      forks: 1, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/skinamarink/5403493395644416/4780582074892288.png', 
+      link: 'https://www.khanacademy.org/computer-programming/skinamarink/5403493395644416'
+    }, 
+    {
+      title: 'Waterfall of Flames | ExDiCode 2023 (Challenge 5: Elemental)', 
+      author: 'CylenceScythe', 
+      votes: 43, 
+      forks: 4, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/waterfall-of-flames-exdicode-2023-challenge-5-elemental/6589064692416512/6413649134796800.png', 
+      link: 'https://www.khanacademy.org/computer-programming/waterfall-of-flames-exdicode-2023-challenge-5-elemental/6589064692416512'
+    }, 
+    {
+      title: 'Into the Clouds [3D Game]', 
+      author: 'Dipper98', 
+      votes: 50, 
+      forks: 4, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/into-the-clouds-3d-game/5107147832868864/6216456499150848.png', 
+      link: 'https://www.khanacademy.org/computer-programming/into-the-clouds-3d-game/5107147832868864'
+    }, 
+    {
+      title: 'fishnado', 
+      author: 'LightningCoder', 
+      votes: 21, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/fishnado/6328407489298432/6649221178769408.png', 
+      link: 'https://www.khanacademy.org/computer-programming/fishnado/6328407489298432'
+    }, 
+    {
+      title: 'cloud interesting effects', 
+      author: 'Prisha B.', 
+      votes: 75, 
+      forks: 13, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/cloud-interesting-effects/5817858687025152/6336163059384320.png', 
+      link: 'https://www.khanacademy.org/computer-programming/cloud-interesting-effects/5817858687025152'
+    }, 
+    {
+      title: 'why you can\'t go outside', 
+      author: 'LovelyPetals', 
+      votes: 27, 
+      forks: 1, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/why-you-cant-go-outside/4733894559842304/6133370709131264.png', 
+      link: 'https://www.khanacademy.org/computer-programming/why-you-cant-go-outside/4733894559842304'
+    }, 
+    {
+      title: 'Duolingo! [No PJS]', 
+      author: 'Fire Wolf', 
+      votes: 52, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/duolingo-no-pjs/5501996021104640/4950807856594944.png', 
+      link: 'https://www.khanacademy.org/computer-programming/duolingo-no-pjs/5501996021104640'
+    }, 
+    {
+      title: 'African Painted (Wild) Dog PIXEL', 
+      author: 'PenGwen', 
+      votes: 13, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/african-painted-wild-dog-pixel/6389379516121088/4928273115299840.png', 
+      link: 'https://www.khanacademy.org/computer-programming/african-painted-wild-dog-pixel/6389379516121088'
+    }, 
+    {
+      title: 'my fastest sanest metaballs. [kinda legal?]', 
+      author: 'YokieBob', 
+      votes: 18, 
+      forks: 1, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/my-fastest-sanest-metaballs-kinda-legal/4792114385043456/4789119798788096.png', 
+      link: 'https://www.khanacademy.org/computer-programming/my-fastest-sanest-metaballs-kinda-legal/4792114385043456'
+    }, 
+    {
+      title: 'Math.ka (V1.2)', 
+      author: '21262', 
+      votes: 54, 
+      forks: 3, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/mathka-v12/5679116412305408/5913609547071488.png', 
+      link: 'https://www.khanacademy.org/computer-programming/mathka-v12/5679116412305408'
+    }, 
+    {
+      title: 'Two Truths & One Lie Pt. 2 of 4', 
+      author: 'J-J B (Offline) ', 
+      votes: 9, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/two-truths-one-lie-pt-2-of-4/5613613312425984/5476443633270784.png', 
+      link: 'https://www.khanacademy.org/computer-programming/two-truths-one-lie-pt-2-of-4/5613613312425984'
+    }, 
+    {
+      title: 'Can you guess the number?', 
+      author: 'Animation Guy', 
+      votes: 18, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/can-you-guess-the-number/5750316702416896/5842552253366272.png', 
+      link: 'https://www.khanacademy.org/computer-programming/can-you-guess-the-number/5750316702416896'
+    }, 
+    {
+      title: 'EMPIRE WARS [FULL RELEASE]', 
+      author: 'LightningCoder', 
+      votes: 104, 
+      forks: 12, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/empire-wars-full-release/5609492106756096/5694592173522944.png', 
+      link: 'https://www.khanacademy.org/computer-programming/empire-wars-full-release/5609492106756096'
+    }, 
+    {
+      title: 'Secret Blocks', 
+      author: 'MaXX', 
+      votes: 39, 
+      forks: 28, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/secret-blocks/4513331392331776/4517660367044608.png', 
+      link: 'https://www.khanacademy.org/computer-programming/secret-blocks/4513331392331776'
+    }, 
+    {
+      title: 'Anti-aliasing for P3D', 
+      author: 'Dat', 
+      votes: 45, 
+      forks: 3, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/anti-aliasing-for-p3d/5688356149444608/4799600525623296.png', 
+      link: 'https://www.khanacademy.org/computer-programming/anti-aliasing-for-p3d/5688356149444608'
+    }, 
+    {
+      title: 'Undertale (Khan Edition) Improved!', 
+      author: 'Undertale Studios', 
+      votes: 27, 
+      forks: 2, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/undertale-khan-edition-improved/4732676479434752/5751392105185280.png', 
+      link: 'https://www.khanacademy.org/computer-programming/undertale-khan-edition-improved/4732676479434752'
+    }, 
+    {
+      title: 'DVD screensaver', 
+      author: 'Ducky Momo', 
+      votes: 8, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/dvd-screensaver/4779457061896192/5011782815236096.png', 
+      link: 'https://www.khanacademy.org/computer-programming/dvd-screensaver/4779457061896192'
+    }, 
+    {
+      title: 'Biscuit Baker', 
+      author: 'Jed2009', 
+      votes: 13, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/biscuit-baker/4829520677093376/5577069549633536.png', 
+      link: 'https://www.khanacademy.org/computer-programming/biscuit-baker/4829520677093376'
+    }, 
+    {
+      title: 'LJE\'s Subbie page', 
+      author: 'LJE', 
+      votes: 6, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/ljes-subbie-page/5696182250946560/4771895688085504.png', 
+      link: 'https://www.khanacademy.org/computer-programming/ljes-subbie-page/5696182250946560'
+    }, 
+    {
+      title: 'Isometric Tilemap template', 
+      author: 'apex [bio]', 
+      votes: 17, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/isometric-tilemap-template/5601723551105024/4529368380784640.png', 
+      link: 'https://www.khanacademy.org/computer-programming/isometric-tilemap-template/5601723551105024'
+    }, 
+    {
+      title: 'Flashlight guy', 
+      author: 'Viper', 
+      votes: 12, 
+      forks: 2, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/flashlight-guy/6134977853177856/4522220783452160.png', 
+      link: 'https://www.khanacademy.org/computer-programming/flashlight-guy/6134977853177856'
+    }, 
+    {
+      title: 'Derivao de "Projeto: estrela cadente"', 
+      author: 'ana-lauramunhoz', 
+      votes: 24, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/derivao-de-projeto-estrela-cadente/6074522229063680/6576501967601664.png', 
+      link: 'https://www.khanacademy.org/computer-programming/derivao-de-projeto-estrela-cadente/6074522229063680'
+    }, 
+    {
+      title: 'Natureopolis', 
+      author: 'CoraL', 
+      votes: 51, 
+      forks: 3, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/natureopolis/5733649343627264/4723081925148672.png', 
+      link: 'https://www.khanacademy.org/computer-programming/natureopolis/5733649343627264'
+    }, 
+    {
+      title: 'Generate a Color Scheme from an Image', 
+      author: 'Paperwad ', 
+      votes: 5, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/generate-a-color-scheme-from-an-image/5736115661488128/5420675261317120.png', 
+      link: 'https://www.khanacademy.org/computer-programming/generate-a-color-scheme-from-an-image/5736115661488128'
+    }, 
+    {
+      title: 'Hemi', 
+      author: 'KarLlr', 
+      votes: 54, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/hemi/5637714001313792/5979630777712640.png', 
+      link: 'https://www.khanacademy.org/computer-programming/hemi/5637714001313792'
+    }, 
+    {
+      title: 'Deja-vu: part 10, Bad Company', 
+      author: 'Cookies \'n Scream', 
+      votes: 23, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/deja-vu-part-10-bad-company/6593453062668288/4958409525084160.png', 
+      link: 'https://www.khanacademy.org/computer-programming/deja-vu-part-10-bad-company/6593453062668288'
+    }, 
+    {
+      title: 'my 1st platformer :D', 
+      author: '187817', 
+      votes: 27, 
+      forks: 1, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/my-1st-platformer-d/5120056961056768/5124447222939648.png', 
+      link: 'https://www.khanacademy.org/computer-programming/my-1st-platformer-d/5120056961056768'
+    }, 
+    {
+      title: 'Spin-off of "Project: Animal attack"', 
+      author: 'madalyn.dower', 
+      votes: 6, 
+      forks: 1, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/spin-off-of-project-animal-attack/5956328298430464/5990284209209344.png', 
+      link: 'https://www.khanacademy.org/computer-programming/spin-off-of-project-animal-attack/5956328298430464'
+    }, 
+    {
+      title: 'KA Notifications Chrome Extension', 
+      author: 'Polar', 
+      votes: 24, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/ka-notifications-chrome-extension/6695638769385472/6428995342352384.png', 
+      link: 'https://www.khanacademy.org/computer-programming/ka-notifications-chrome-extension/6695638769385472'
+    }, 
+    {
+      title: 'Polygon Collider', 
+      author: 'Polar', 
+      votes: 33, 
+      forks: 0, 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/polygon-collider/6029125511069696/4936959271419904.png', 
+      link: 'https://www.khanacademy.org/computer-programming/polygon-collider/6029125511069696'
+    }, 
+  ];
+};
