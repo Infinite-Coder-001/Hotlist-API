4d2cf03dc82b2ec814e24844398fad4b8c9a552e
Infinite coder <123761858+Infinite-Coder-001@users.noreply.github.com>
2023-03-11 22:00:44 +0100

    NEW version

---
+++ remove.py
@@ -0,0 +1,12 @@
+# This script is used in NEW version
+# It removes old API logs
+
+for i in range(3, 600, 1):
+    fullOldDate = (datetime.datetime.utcnow() - datetime.timedelta(minutes = i)).strftime('%Y%m%d%H%M')
+    fullOldDateSplited = list(fullOldDate)
+    for j in range(0, 10):
+      oldFileName = "new-api/new-api-" + fullOldDateSplited[0] + fullOldDateSplited[1] + fullOldDateSplited[2] + fullOldDateSplited[3] + fullOldDateSplited[4] + fullOldDateSplited[5] + fullOldDateSplited[6] + fullOldDateSplited[7] + fullOldDateSplited[8] + fullOldDateSplited[9] + fullOldDateSplited[10] + fullOldDateSplited[11] + "-" + str(j) + ".js"
+      try:
+          os.remove(oldFileName)
+      except:
+          pass
