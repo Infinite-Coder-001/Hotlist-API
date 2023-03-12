62f91737330db6c74dd7a0e272f6a57df45b8223
Infinite coder <123761858+Infinite-Coder-001@users.noreply.github.com>
2023-03-12 10:37:13 +0100

    NEW version

--- remove.py
+++ remove.py
@@ -4,10 +4,10 @@
 import datetime
 import os
 
-for i in range(3, 600, 1):
+for i in range(3, 6000, 1):
     fullOldDate = (datetime.datetime.utcnow() - datetime.timedelta(minutes = i)).strftime('%Y%m%d%H%M')
     fullOldDateSplited = list(fullOldDate)
-    for j in range(0, 10):
+    for j in range(0, 3):
       oldFileName = "new-api/new-api-" + fullOldDateSplited[0] + fullOldDateSplited[1] + fullOldDateSplited[2] + fullOldDateSplited[3] + fullOldDateSplited[4] + fullOldDateSplited[5] + fullOldDateSplited[6] + fullOldDateSplited[7] + fullOldDateSplited[8] + fullOldDateSplited[9] + fullOldDateSplited[10] + fullOldDateSplited[11] + "-" + str(j) + ".js"
       print(oldFileName)
       try:
