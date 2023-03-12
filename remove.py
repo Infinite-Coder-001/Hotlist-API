9028976b26cd882d8cd5b8522d95fe4ba7b176e6
Infinite coder <123761858+Infinite-Coder-001@users.noreply.github.com>
2023-03-12 15:50:35 +0100

    NEW version

--- remove.py
+++ remove.py
@@ -7,11 +7,9 @@ import os
 for i in range(3, 600, 1):
     fullOldDate = (datetime.datetime.utcnow() - datetime.timedelta(minutes = i)).strftime('%Y%m%d%H%M')
     fullOldDateSplited = list(fullOldDate)
-    for j in range(0, 3):
-      oldFileName = "new-api/new-api-" + fullOldDateSplited[0] + fullOldDateSplited[1] + fullOldDateSplited[2] + fullOldDateSplited[3] + fullOldDateSplited[4] + fullOldDateSplited[5] + fullOldDateSplited[6] + fullOldDateSplited[7] + fullOldDateSplited[8] + fullOldDateSplited[9] + fullOldDateSplited[10] + fullOldDateSplited[11] + "-" + str(j) + ".js"
-      print(oldFileName)
-      try:
-          os.remove(oldFileName)
-          print("Removed")
-      except:
-          pass
+    oldFileName = "new-api/new-api-" + fullOldDateSplited[0] + fullOldDateSplited[1] + fullOldDateSplited[2] + fullOldDateSplited[3] + fullOldDateSplited[4] + fullOldDateSplited[5] + fullOldDateSplited[6] + fullOldDateSplited[7] + fullOldDateSplited[8] + fullOldDateSplited[9] + fullOldDateSplited[10] + fullOldDateSplited[11] + ".js"
+    try:
+        os.remove(oldFileName)
+        print("Removed")
+    except:
+        pass
