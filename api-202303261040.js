fc102a159cf4d84818d3a581bbc757abd1805dac
Infinite-Coder-001 <Infinite-Coder-001@users.noreply.github.com>
2023-03-26 10:48:16 +0000

    .[BOT] 1.3.0 - Update hotlist data

--- api-202303261040.js
+++ api-202303261040.js
@@ -101,7 +101,7 @@ var LoadHotlistAPI = function() {
     }, 
     {
       title: 'T-Rex art _| using curveVertex tool', 
-      author: 'CodingChamp01 🟢', 
+      author: 'CodingChamp01 🔴', 
       votes: 19, 
       forks: 1, 
       thumbnail: 'https://www.khanacademy.org/computer-programming/t-rex-art-_-using-curvevertex-tool/5691285156708352/6640704753549312.png', 
