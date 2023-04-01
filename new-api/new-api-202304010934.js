5bc347150f0bf44dd6c4b06b4101f1179b320d45
Infinite coder <123761858+Infinite-Coder-001@users.noreply.github.com>
2023-04-01 11:34:33 +0200

    [BOT] Update hotlist data

--- new-api/new-api-202304010934.js
+++ new-api/new-api-202304010934.js
@@ -320,7 +320,7 @@ var LoadHotlistAPI = function() {
       author: 'Velvet', 
       votes: 6, 
       forks: 0, 
-      thumbnail: 'https://www.khanacademy.org/computer-programming/new-webpage/6403515498151936/5660253556621312.png', 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/new-webpage/6403515498151936/6029810393726976.png', 
       link: 'https://www.khanacademy.org/computer-programming/new-webpage/6403515498151936', 
       authorLink: 'https://www.khanacademy.org/profile/kaid_9718267243517170072128027'
     }, 
