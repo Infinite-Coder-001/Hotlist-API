36a4884e42079a193d2014f8ff44589703da5e37
Infinite coder <123761858+Infinite-Coder-001@users.noreply.github.com>
2023-04-04 08:07:34 +0200

    [BOT] Update hotlist data

--- new-api/new-api-202304040607.js
+++ new-api/new-api-202304040607.js
@@ -39,7 +39,7 @@ var LoadHotlistAPI = function() {
     {
       title: 'Free Form Tower Defense', 
       author: 'Larry Serflaten', 
-      votes: 79, 
+      votes: 80, 
       forks: 5, 
       thumbnail: 'https://www.khanacademy.org/computer-programming/free-form-tower-defense/6317378653962240/5547245861912576.png', 
       link: 'https://www.khanacademy.org/computer-programming/free-form-tower-defense/6317378653962240', 
