e854205a3c5a3e0767367bdffadc5cc22a286a4e
Infinite-Coder-001 <Infinite-Coder-001@users.noreply.github.com>
2023-04-04 04:58:24 +0000

    .[BOT] 1.3.0 - Update hotlist data

--- api-202304040450.js
+++ api-202304040450.js
@@ -77,7 +77,7 @@ var LoadHotlistAPI = function() {
       author: 'Xiøn😋🤩', 
       votes: 21, 
       forks: 0, 
-      thumbnail: 'https://www.khanacademy.org/computer-programming/-campfire-includes-link-to-vertex-tool/5768548204199936/5189916268216320.png', 
+      thumbnail: 'https://www.khanacademy.org/computer-programming/-campfire-includes-link-to-vertex-tool/5768548204199936/4936278987816960.png', 
       link: 'https://www.khanacademy.org/computer-programming/-campfire-includes-link-to-vertex-tool/5768548204199936', 
       authorLink: 'https://www.khanacademy.org/profile/kaid_948710635020586603248429'
     }, 
