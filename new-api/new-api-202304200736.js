b9a705b2a0447d22bb753d61eab76c4229d39d93
Infinite coder <123761858+Infinite-Coder-001@users.noreply.github.com>
2023-04-20 09:36:35 +0200

    [BOT] Update hotlist data

--- new-api/new-api-202304200736.js
+++ new-api/new-api-202304200736.js
@@ -21,7 +21,7 @@ var LoadHotlistAPI = function() {
     {
       title: 'Pegs [game]', 
       author: 'Cataclysmic Code 🌩‮', 
-      votes: 47, 
+      votes: 46, 
       forks: 2, 
       thumbnail: 'https://www.khanacademy.org/computer-programming/pegs-game/5437352526004224/5407377345265664.png', 
       link: 'https://www.khanacademy.org/computer-programming/pegs-game/5437352526004224', 
