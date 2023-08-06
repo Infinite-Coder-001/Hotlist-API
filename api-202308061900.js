390218df1c05ed2e019cdf4bad1b7554011c31b5
Infinite coder <123761858+Infinite-Coder-001@users.noreply.github.com>
2023-08-06 21:00:06 +0200

    Hotlist - 1.3.0 - update - 06-08-2023 19:00

---
+++ api-202308061900.js
@@ -0,0 +1,32 @@
+var LoadHotlistAPI = function() {
+  return [
+    {
+      "title": "go touch some grass",
+      "author": "Bearkirb314🐻‍❄️",
+      "votes": 42,
+      "forks": 0,
+      "thumbnail": "https://www.khanacademy.org/computer-programming/go-touch-some-grass/5599085380550656/5012888446877696.png",
+      "link": "https://www.khanacademy.org/computer-programming/go-touch-some-grass/5599085380550656",
+      "authorLink": "https://www.khanacademy.org/profile/kaid_375460112550893828689953"
+    },
+    {
+      "title": "Baby Bunny",
+      "author": "JLE",
+      "votes": 42,
+      "forks": 1,
+      "thumbnail": "https://www.khanacademy.org/computer-programming/baby-bunny/5753145236701184/6709374792548352.png",
+      "link": "https://www.khanacademy.org/computer-programming/baby-bunny/5753145236701184",
+      "authorLink": "https://www.khanacademy.org/profile/kaid_344949764307318233637518"
+    },
+    {
+      "title": "Matcap test",
+      "author": "Edward",
+      "votes": 15,
+      "forks": 1,
+      "thumbnail": "https://www.khanacademy.org/computer-programming/matcap-test/6662422713384960/5156474874281984.png",
+      "link": "https://www.khanacademy.org/computer-programming/matcap-test/6662422713384960",
+      "authorLink": "https://www.khanacademy.org/profile/kaid_673263829029683148315313"
+    },
+    {
+      "title": "Stranded Khan [GAME]",;
+};
